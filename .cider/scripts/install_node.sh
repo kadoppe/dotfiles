ab444828bc76a81bfc6239c93a7c96fabe60eaf7
kadoppe <kadoppe@me.com>
2015-03-09 20:38:54 +0900

    Remove Cider files

--- .cider/scripts/install_node.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-nodebrew install-binary latest --skip-existing
-nodebrew use latest
-
-npm install -g hubot
-npm install -g coffee-script
-npm install -g yo
-npm install -g generator-hubot
--- .cider/scripts/launchctl.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
