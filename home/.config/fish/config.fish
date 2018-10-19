81c08e624e2b36f3fab24d4a111ca496d376df51
kadoppe <kadoppe@me.com>
2018-10-19 11:40:56 +0900

    setup direnv

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -13,6 +13,8 @@ status --is-interactive; and source (pyenv virtualenv-init -|psub)
 alias vi=vim
 alias mux=tmuxinator
 
+eval (direnv hook fish)
+
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \c] peco_select_ghq_repository
