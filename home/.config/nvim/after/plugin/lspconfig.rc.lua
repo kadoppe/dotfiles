c49c16139dfb4bc20673c17a468fae575bed0a65
kadoppe <kadoppe@me.com>
2022-07-20 22:54:33 +0900

    Install vimls

--- home/.config/nvim/after/plugin/lspconfig.rc.lua
+++ home/.config/nvim/after/plugin/lspconfig.rc.lua
@@ -21,6 +21,11 @@ lspconfig.tsserver.setup{
   capabilities = capabilities
 }
 
+lspconfig.vimls.setup{
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
 lspconfig.sumneko_lua.setup{
   on_attach = on_attach,
   capabilities = capabilities,
