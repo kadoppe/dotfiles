1bf0f8c01475bf19f0f9dba51189cf32bfcf13fb
kadoppe <kadoppe@me.com>
2022-07-20 22:19:15 +0900

    modify lualine settings

--- home/.config/nvim/after/plugin/lualine.rc.lua
+++ home/.config/nvim/after/plugin/lualine.rc.lua
@@ -11,7 +11,11 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{
+      'filename',
+      file_status: true,
+      path = 0
+    }},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -19,7 +23,11 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{
+      'filename',
+      file_status: true,
+      path = 1
+    }},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
