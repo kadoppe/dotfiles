7c857f198a18a31f01f07c4e7f93f6be943f2a2b
kadoppe <kadoppe@me.com>
2022-07-21 12:17:25 +0900

    refine config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,25 +1,44 @@
-set -x EDITOR 'vim'
+set fish_greeting ""
 
-set PATH /opt/homebrew/bin $PATH
-set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin
+set -gx TERM xterm-256color
 
-set -x CPPFLAGS -I/usr/local/opt/openssl/include
-set -x LDFLAGS -L/usr/local/opt/openssl/lib
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+set -g theme_nerd_fonts yes
 
-set -g theme_color_scheme terminal
-
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
-
+# aliases
 alias vi=nvim
 alias vim=nvim
 alias mux=tmuxinator
 alias lzd=lazydocker
 
+set -gx EDITOR 'nvim'
+
+# path
+set PATH /opt/homebrew/bin $PATH
+
+# go
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
+# pyenv
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
+# openssl
+set -x CPPFLAGS -I/usr/local/opt/openssl/include
+set -x LDFLAGS -L/usr/local/opt/openssl/lib
+
+# direnv
 eval (direnv hook fish)
 
+# peco
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
   bind \c] peco_select_ghq_repository
 end
+
