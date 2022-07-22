1918c0514886197335a6ca6050c507f8dc0625b9
kadoppe <kadoppe@me.com>
2022-07-22 13:32:05 +0900

    chamge theme settings

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,12 +3,9 @@ set fish_greeting ""
 set -gx TERM xterm-256color
 
 # theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
 set -g theme_nerd_fonts yes
+set -g theme_color_scheme nord
+set -g theme_date_format "+ %Y/%m/%d %H:%M"
 
 # aliases
 alias vi=nvim
