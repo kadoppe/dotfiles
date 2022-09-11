9c51e48d2e6602294354b3ca84d26a6e2ebd4812
kadoppe <kadoppe@me.com>
2022-09-11 22:58:44 +0900

    modify lspsaga settings

--- home/.config/nvim/after/plugin/lspsaga.rc.lua
+++ home/.config/nvim/after/plugin/lspsaga.rc.lua
@@ -17,14 +17,6 @@ end, { silent = true, noremap =true })
 -- Hover doc
 vim.keymap.set("n", "K", require("lspsaga.hover").render_hover_doc, { silent = true })
 
-local action = require("lspsaga.action")
-vim.keymap.set("n", "<C-f>", function()
-  action.smart_scroll_with_saga(1)
-end, { silent = true })
-vim.keymap.set("n", "<C-b>", function()
-  action.smart_scroll_with_saga(-1)
-end, { silent = true })
-
 -- Jump and show diagnostics
 vim.keymap.set("n", "<leader>cd", require("lspsaga.diagnostic").show_line_diagnostics, { silent = true,noremap = true })
 vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true,noremap= true })
@@ -41,7 +33,7 @@ vim.keymap.set("n", "]E", function()
 end, { silent = true, noremap = true })
 
 -- Preview definition
-vim.keymap.set("n", "gp", require("lspsaga.definition").preview_definition, { silent = true, noremap = true })
+vim.keymap.set("n", "gp", require("lspsaga.definition").peek_definition, { silent = true, noremap = true })
 
 -- Rename
 vim.keymap.set("n", "gr", require("lspsaga.rename").lsp_rename, { silent = true,noremap = true })
