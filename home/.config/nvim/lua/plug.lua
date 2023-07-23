c2cfdf3ec9a344aa46a335db589cfe15cbcc4b5d
kadoppe <kadoppe@me.com>
2023-07-23 15:19:59 +0900

    install textlint

--- home/.config/nvim/lua/plug.lua
+++ home/.config/nvim/lua/plug.lua
@@ -390,7 +390,9 @@ require("lazy").setup({
           null_ls.builtins.formatting.black,
           null_ls.builtins.formatting.isort,
           null_ls.builtins.formatting.autoflake,
+          null_ls.builtins.formatting.textlint.with({ filetypes = { "markdown" } }),
           null_ls.builtins.diagnostics.flake8,
+          null_ls.builtins.diagnostics.textlint.with({ filetypes = { "markdown" } }),
         },
       })
     end
