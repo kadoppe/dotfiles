e9c508af01f2521c2791c7d517eacb6ab920511a
kadoppe <kadoppe@me.com>
2023-10-14 14:04:47 +0900

    rmove indent-blankline

--- home/.config/nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("indent_blankline").setup {
-      show_current_context = true,
-    }
-  end
-}
