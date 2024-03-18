8fd4e579936684719a90e4e1357e9fec62f2c031
kadoppe <kadoppe@me.com>
2024-03-18 16:59:23 +0900

    add neovim plugins

--- home/.config/nvim/lua/plugins/mini.lua
+++ home/.config/nvim/lua/plugins/mini.lua
@@ -4,5 +4,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function ()
     require('mini.cursorword').setup()
+    require('mini.pairs').setup()
+    require('mini.surround').setup()
   end
 }
