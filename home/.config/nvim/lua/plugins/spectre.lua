74349df1f0e1290865e925f01060ef74d650a66c
kadoppe <kadoppe@me.com>
2024-08-11 18:19:45 +0900

    use gnu-sed instead

--- home/.config/nvim/lua/plugins/spectre.lua
+++ home/.config/nvim/lua/plugins/spectre.lua
@@ -2,11 +2,34 @@ return {
   "nvim-pack/nvim-spectre",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
+    "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons"
   },
-  -- TODO:
   config = function()
-    require('spectre').setup()
+    require('spectre').setup({
+      replace_engine={
+        ['sed']={
+          cmd = "gsed",
+          args = nil,
+          options = {
+            ['ignore-case'] = {
+              value= "--ignore-case",
+              icon="[I]",
+              desc="ignore case"
+            },
+          }
+        },
+      },
+      default = {
+        find = {
+          cmd = "rg",
+          options = {"ignore-case"}
+        },
+        replace={
+          cmd = "sed"
+        }
+      },
+    })
     vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
       desc = "Toggle Spectre"
     })
