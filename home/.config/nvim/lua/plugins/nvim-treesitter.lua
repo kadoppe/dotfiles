5feb229aac0dc1bae6934807c603c559f40c5346
kadoppe <kadoppe@me.com>
2024-08-18 22:07:39 +0900

    remove powershell settings

--- home/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ home/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,16 +3,6 @@ return {
   build = ":TSUpdate",
   event = "VeryLazy",
   config = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.powershell = {
-      install_info = {
-        url = "https://github.com/jrsconfitto/tree-sitter-powershell",
-        files = {"src/parser.c"}
-      },
-      filetype = "ps1",
-      used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" }
-    }
-
     require('nvim-treesitter.configs').setup {
       highlight = {
         enable = true,
