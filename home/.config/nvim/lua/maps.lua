f5910e882ba5af16fc7d586c1494edf9f811dec5
kadoppe <kadoppe@me.com>
2024-10-27 20:36:42 +0900

    disable recording

--- home/.config/nvim/lua/maps.lua
+++ home/.config/nvim/lua/maps.lua
@@ -4,3 +4,5 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', {noremap = true})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {noremap = true})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {noremap = true})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {noremap = true})
+
+vim.api.nvim_set_keymap('n', 'q', '<Nop>', { noremap = true, silent = true })
