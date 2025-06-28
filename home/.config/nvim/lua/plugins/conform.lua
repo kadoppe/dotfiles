701cdee39820f1be4355e0a349946ae037dc3f4e
kadoppe <kadoppe@me.com>
2025-06-28 19:50:30 +0900

    fix python settings

---
+++ home/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+  'stevearc/conform.nvim',
+  event = "VeryLazy",
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        python = {
+          -- To fix auto-fixable lint errors.
+          "ruff_fix",
+          -- To run the Ruff formatter.
+          "ruff_format",
+          -- To organize the imports.
+          "ruff_organize_imports",
+        },
+      },
+    })
+  end
+}
