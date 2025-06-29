aeddf5971c26e3f7e4e68d62069884c1fc1ab0e5
kadoppe <kadoppe@me.com>
2025-06-29 15:15:36 +0900

    disable markdown formatter

--- home/.config/nvim/lua/plugins/nvim-lint.lua
+++ home/.config/nvim/lua/plugins/nvim-lint.lua
@@ -2,9 +2,16 @@ return {
   "mfussenegger/nvim-lint",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
+    local lint = require("lint")
+    
+    -- リンターの設定（必要に応じて追加）
+    lint.linters_by_ft = {
+      -- markdown = {}, -- Markdownのリンターを無効化
+    }
+    
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
-        require("lint").try_lint()
+        lint.try_lint()
       end,
     })
   end
