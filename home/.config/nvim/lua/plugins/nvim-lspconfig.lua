2363d344e57c5033f9cf91e80507ed513cfcd7d1
kadoppe <kadoppe@me.com>
2025-06-29 15:15:59 +0900

    organize lspconfig config

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -14,7 +14,8 @@ return {
             "ts_ls",
             "vimls",
             "astro",
-            "powershell_es",
+            "pyright",
+            "ruff",
             "tailwindcss",
             "terraformls",
           },
@@ -71,116 +72,133 @@ return {
       }
     }
 
-    if vim.fn.executable("deno") then
-      lspconfig.denols.setup {
-        on_attach = on_attach,
-        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-        init_options = {
-          enable = true,
-          lint = true,
-          unstable = true,
-          suggest = {
-            imports = {
-              hosts = {
-                ["https://deno.land"] = true,
-                ["https://cdn.nest.land"] = true,
-                ["https://crux.land"] = true,
-              },
-            },
-          },
-        },
-        capabilities = capabilities,
-      }
-    end
+    -- if vim.fn.executable("deno") then
+    --   lspconfig.denols.setup {
+    --     on_attach = on_attach,
+    --     root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+    --     init_options = {
+    --       enable = true,
+    --       lint = true,
+    --       unstable = true,
+    --       suggest = {
+    --         imports = {
+    --           hosts = {
+    --             ["https://deno.land"] = true,
+    --             ["https://cdn.nest.land"] = true,
+    --             ["https://crux.land"] = true,
+    --           },
+    --         },
+    --       },
+    --     },
+    --     capabilities = capabilities,
+    --   }
+    -- end
 
-    lspconfig.gopls.setup {
-      on_attach = on_attach,
-      capabilities = capabilities
-    }
+    -- lspconfig.gopls.setup {
+    --   on_attach = on_attach,
+    --   capabilities = capabilities
+    -- }
 
-    local autocmd = vim.api.nvim_create_autocmd
-    autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-        local params = vim.lsp.util.make_range_params()
-        params.context = {only = {"source.organizeImports"}}
-        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-        -- machine and codebase, you may want longer. Add an additional
-        -- argument after params if you find that you have to write the file
-        -- twice for changes to be saved.
-        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-        for cid, res in pairs(result or {}) do
-          for _, r in pairs(res.result or {}) do
-            if r.edit then
-              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-              vim.lsp.util.apply_workspace_edit(r.edit, enc)
-            end
-          end
-        end
-        vim.lsp.buf.format({async = false})
-      end
-    })
+    -- local autocmd = vim.api.nvim_create_autocmd
+    -- autocmd("BufWritePre", {
+    --   pattern = "*.go",
+    --   callback = function()
+    --     local params = vim.lsp.util.make_range_params()
+    --     params.context = {only = {"source.organizeImports"}}
+    --     -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    --     -- machine and codebase, you may want longer. Add an additional
+    --     -- argument after params if you find that you have to write the file
+    --     -- twice for changes to be saved.
+    --     -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    --     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    --     for cid, res in pairs(result or {}) do
+    --       for _, r in pairs(res.result or {}) do
+    --         if r.edit then
+    --           local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+    --           vim.lsp.util.apply_workspace_edit(r.edit, enc)
+    --         end
+    --       end
+    --     end
+    --     vim.lsp.buf.format({async = false})
+    --   end
+    -- })
 
-    lspconfig.pylsp.setup {
+    vim.lsp.enable('pyright')
+    vim.lsp.config('pyright', {
       on_attach = on_attach,
       capabilities = capabilities,
       settings = {
-        pylsp = {
-          plugins = {
-            -- format
-            autopep8 = {enabled = true},
-            pycodestyle = {enabled = false},
-            -- type check
-            pylsp_mypy = {enabled = true},
-            -- lint
-            pyflakes = {enabled = false},
-            mccabe = {enabled = false},
-          }
-        }
-      }
-    }
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            -- ignore = { '*' },
+          },
+        },
+      },
+    })
 
-    lspconfig.vimls.setup {
+    vim.lsp.enable('ruff')
+    vim.lsp.config('ruff', {
       on_attach = on_attach,
       capabilities = capabilities,
-    }
+      cmd = { 'uv', 'run', 'ruff', 'server' },
+    })
 
-    lspconfig.lua_ls.setup {
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then
+          return
+        end
+        if client.name == 'ruff' then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end,
+      desc = 'LSP: Disable hover capability from Ruff',
+    })
+
+
+
+    vim.lsp.enable('vimls')
+    vim.lsp.config('vimls', {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable('lua_ls')
+    vim.lsp.config('lua_ls', {
       on_attach = on_attach,
       capabilities = capabilities,
       settings = {
         Lua = {
           diagnostics = {
             globals = { 'vim' }
-          }
+          },
         }
       }
-    }
+    })
 
-    lspconfig.powershell_es.setup {
+    vim.lsp.enable('tailwindcss')
+    vim.lsp.config('tailwindcss', {
       on_attach = on_attach,
-      capabilities = capabilities
-    }
+      capabilities = capabilities,
+    })
 
-    lspconfig.astro.setup {
+    vim.lsp.enable('terraformls')
+    vim.lsp.config('terraformls', {
       on_attach = on_attach,
-      capabilities = capabilities
-    }
+      capabilities = capabilities,
+    })
 
-    lspconfig.tailwindcss.setup {
-      on_attach = on_attach,
-      capabilities = capabilities
-    }
-
-    lspconfig.terraformls.setup {
-      on_attach = on_attach,
-      capabilities = capabilities
-    }
-
-    lspconfig.solargraph.setup {
-      on_attach = on_attach,
-      capabilities = capabilities
-    }
+    -- lspconfig.solargraph.setup {
+    --   on_attach = on_attach,
+    --   capabilities = capabilities
+    -- }
   end
 }
