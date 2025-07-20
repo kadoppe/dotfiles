f23d4d437c3a44c9adf800376f60edbefb0d2111
kadoppe <kadoppe@me.com>
2025-07-20 15:00:56 +0900

    fix windsurf * cmp

--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -39,11 +39,9 @@ return {
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
-        { name = "codeium" },
+        { name = 'codeium' },
         { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
         { name = 'path' },
-        { name = 'nvim_lua' },
       }, {
         { name = 'buffer' },
       }),
@@ -52,25 +50,9 @@ return {
           mode = 'symbol_text'
         })
       },
-      sorting = {
-        priority_weight = 2,
-        comparators = {
-          -- Below is the default comparitor list and order for nvim-cmp
-          cmp.config.compare.offset,
-          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.locality,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
-      },
     })
 
-    cmp.setup.cmdline({'/', '?'}, {
+    cmp.setup.cmdline({ '/', '?' }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
         { name = 'buffer' }
@@ -83,24 +65,8 @@ return {
         { name = 'path' }
       }, {
         { name = 'cmdline' },
-      })
-    })
-
-    local has_words_before = function()
-      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-    end
-    cmp.setup({
-      mapping = {
-        ["<Tab>"] = vim.schedule_wrap(function(fallback)
-          if cmp.visible() and has_words_before() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end),
-      },
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false }
     })
   end
 }
