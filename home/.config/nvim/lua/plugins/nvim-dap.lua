a0ef099ac7ca17b81eb6c92cf42ffd0983c3e963
kadoppe <kadoppe@me.com>
2025-07-26 16:08:59 +0900

    setup nvim dap settings

--- home/.config/nvim/lua/plugins/nvim-dap.lua
+++ home/.config/nvim/lua/plugins/nvim-dap.lua
@@ -2,11 +2,28 @@ return {
   'mfussenegger/nvim-dap',
   event = "VimEnter",
   dependencies = {
+    'rcarriga/nvim-dap-ui',
+    "nvim-neotest/nvim-nio",
+    "jay-babu/mason-nvim-dap.nvim",
+    "theHamsta/nvim-dap-virtual-text",
     'mfussenegger/nvim-dap-python',
   },
   config = function()
     local dap = require('dap')
+    local ui = require("dapui")
+    local dap_virtual_text = require("nvim-dap-virtual-text")
+
+    dap_virtual_text.setup()
+
     require("dap-python").setup("uv")
+    require('dap').configurations.python = {
+      {
+        name    = 'Attach FastAPI',
+        type    = 'python',
+        request = 'attach',
+        connect = { host = '127.0.0.1', port = 5678 },
+      },
+    }
 
     vim.keymap.set('n', '<leader>db', function()
       dap.toggle_breakpoint()
@@ -33,5 +50,19 @@ return {
     end, {
       desc = "Dap: REPL open"
     })
+
+    ui.setup()
+    dap.listeners.before.attach.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      ui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      ui.close()
+    end
   end
 }
