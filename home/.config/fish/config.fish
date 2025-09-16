569b8a369a66b23722c423cddaa50736619a2896
kadoppe <kadoppe@me.com>
2025-09-16 09:29:32 +0900

    add alias for codex

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -9,6 +9,7 @@ alias ls='eza --icons --git'
 alias lt='eza -T -L 3 -a -I "node_modules|.git|.cache" --icons'
 alias ltl='eza -T -L 3 -a -I "node_modules|.git|.cache" -l --icons'
 alias cc='claude --dangerously-skip-permissions'
+alias co='codex --dangerously-bypass-approvals-and-sandbox'
 
 set -gx EDITOR 'nvim'
 
